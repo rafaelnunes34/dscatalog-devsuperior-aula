f43f8235edc8848ca2994bb75faf0f23d75dfe3b
rafaelnunes34 <rafael.nunes34@gmail.com>
2022-08-11 09:16:52 -0300

    Fix: Postgres compatibility

--- backend/src/main/java/com/devsuperior/dscatalog/repositories/ProductRepository.java
+++ backend/src/main/java/com/devsuperior/dscatalog/repositories/ProductRepository.java
@@ -1,9 +1,19 @@
 package com.devsuperior.dscatalog.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.devsuperior.dscatalog.entities.Category;
 import com.devsuperior.dscatalog.entities.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
-
+	
+	@Query("SELECT DISTINCT obj FROM Product obj INNER JOIN obj.categories cats WHERE "
+			+ "(COALESCE(:categories) IS NULL OR cats IN :categories) AND "
+			+ "(LOWER(obj.name) Like LOWER(CONCAT('%',:name,'%')) )")
+	Page<Product> find(List<Category> categories, String name, Pageable pageable);
 }
